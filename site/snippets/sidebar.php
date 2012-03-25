ecc9d89feafb5f1f6c88302330aa8a576c41cdc9
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2012-03-25 19:14:01 +0200

    Refined sidebar

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -11,6 +11,16 @@
     </p>
   </div>
   <div class="sidebar_title">
+    <h3 class="ui">Navigation</h3>
+  </div>
+  <div class="sidebar_content">
+    <ul>
+      <?php foreach($pages->visible() AS $p): ?>
+      <li><a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
+      <?php endforeach ?>
+    </ul>
+  </div>
+  <div class="sidebar_title">
     <h3 class="ui">Information</h3>
   </div>
   <div class="sidebar_content">
@@ -18,17 +28,12 @@
       <dt>University</dt><dd><a href="http://www.zhdk.ch">Z&uuml;rich University of the Arts</a></dd>
       <dt>Institute</dt><dd><a href="http://sdfb.ch/">Swiss Design Institute for Finance and Banking</a></dd>
       <dt>Mentor</dt><dd><a href="http://gmb.zhdk.ch/">Prof. Dr. Gerhard M. Buurman</a></dd>
+      <dt>Advisors</dt>
+      <dd><a href="http://gmb.zhdk.ch/">Prof. Dr. Gerhard M. Buurman</a></dd>
+      <dd><a href="http://infosthetics.com/">Prof. Andrew Vande Moere</a></dd>
+      <dd><a href="http://www.hs-augsburg.de/~mstoll/">Prof. Michael Stoll</a></dd>
+      <dd><a href="http://stamen.com/">Eric Rodenbeck</a></dd>
       <dt>Partner</dt><dd><a href="http://interactivethings.com/">Interactive Things</a></dd>
     </dl>
   </div>
-  <div class="sidebar_title">
-      <h3 class="ui">Navigation</h3>
-    </div>
-    <div class="sidebar_content">
-      <ul>
-        <?php foreach($pages->visible() AS $p): ?>
-        <li><a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
-        <?php endforeach ?>
-      </ul>
-    </div>
 </div>
