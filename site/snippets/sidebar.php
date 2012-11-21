9cd5acfe0d971c6d7c1314734843573ec763cf26
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2012-11-21 11:24:50 +0100

    Added Enrico as an advisor.

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -27,11 +27,13 @@
     <dl>
       <dt>University</dt><dd><a href="http://www.zhdk.ch">Z&uuml;rich University of the Arts</a></dd>
       <dt>Institute</dt><dd><a href="http://sdfb.ch/">Swiss Design Institute for Finance and Banking</a></dd>
-      <dt>Mentor</dt><dd><a href="http://gmb.zhdk.ch/">Prof. Dr. Gerhard M. Buurman</a></dd>
-      <dt>Advisors</dt>
+      <dt>Mentors</dt>
       <dd><a href="http://gmb.zhdk.ch/">Prof. Dr. Gerhard M. Buurman</a></dd>
+      <dd><a href="http://viad.zhdk.ch/de/people/karmen-franinovic">Karmen Franinović</a></dd>
+      <dt>Advisors</dt>
       <dd><a href="http://infosthetics.com/">Prof. Andrew Vande Moere</a></dd>
       <dd><a href="http://www.hs-augsburg.de/~mstoll/">Prof. Michael Stoll</a></dd>
+      <dd><a href="http://enrico.bertini.me/">Prof. Enrico Bertini</a></dd>
       <dd><a href="http://stamen.com/">Eric Rodenbeck</a></dd>
       <dt>Partner</dt><dd><a href="http://interactivethings.com/">Interactive Things</a></dd>
     </dl>
